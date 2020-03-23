b2140c49dc667c8ea8355355f611d9f2a9adb199
Fabian Haglund <till.fabian@gmail.com>
2020-03-23 11:44:14 +0100

    pretty buttons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
+  <div id="app">
     <Start/>
   </div>
 </template>
 
 <script>
-import Start from './components/Start.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Start
+  import Start from './components/Start.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Start
+    }
   }
-}
 </script>
+
+<style>
+
+  #app{
+    padding: 40px;
+  }
+
+  button {
+    margin: 20px; 
+    padding: 10px; 
+    font-family: 'Courier New', Courier, monospace;
+    background-color: paleturquoise ;
+    font-size: 24px;
+  }
+
+</style>
--- src/components/Start.vue
+++ src/components/Start.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
-        Demo MongoDB
+        <div id="buttons">
+            <button>Insert</button>
+            <button>Remove</button>
+            <button>Display</button>
+            <button>Update</button>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Start'
+}
+</script>
+
+<style scoped>
+    #buttons {
+        display: flex; 
+        flex-direction: row;
+    }
+</style>
